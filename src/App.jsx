57eba28cb035e305f8442a5ebbb45634afabc34d
Shreyanshi Verma <shreyanshi.verma200302@gmail.com>
2022-07-11 03:30:17 +0530

    creating profile context

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,26 @@ import SignIn from "./pages/SignIn";
 import PrivateRoute from './components/PrivateRoute';
 import Home from './pages/Home';
 import PublicRoute from "./components/PublicRoute";
+import { ProfileProvider } from "./context/profile.context";
 
 
 function App() {
-  return <Switch>
+  return ( 
+  <ProfileProvider>
+
+  <Switch>
     <PublicRoute path="/signin">
+   
+
       <SignIn />
     </PublicRoute>
     <PrivateRoute path="">
+   
       <Home />
     </PrivateRoute>
   </Switch>
-    
+  </ProfileProvider>
+  );  
   
 }
 
--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 import { Redirect, Route } from 'react-router';
+import {useProfile} from '../context/profile.context';
 
 function PrivateRoute({children, ...routeProps}) {
 
-    const profile = false;
+    const profile = useProfile();
 
     if(!profile) {
         return <Redirect to= "/signin" />
