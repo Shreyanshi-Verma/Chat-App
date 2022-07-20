aa4afe4536686959812b8cd3f4cb6dbb1763b3f9
Shreyanshi Verma <shreyanshi.verma200302@gmail.com>
2022-07-21 00:05:17 +0530

    added realtime presence for a user

--- src/components/dashboard/DashboardToggle.jsx
+++ src/components/dashboard/DashboardToggle.jsx
@@ -4,7 +4,8 @@ import React, { useCallback } from 'react';
 import { Button, Icon, Drawer, Alert } from 'rsuite';
 import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
 import Dashboard from '.';
-import { auth } from '../../misc/firebase';
+import { auth, database } from '../../misc/firebase';
+import { isOfflineForDatabase } from '../../context/profile.context';
 
 const DashboardToggle = () => {
   const { isOpen, close, open } = useModalState();
@@ -12,25 +13,39 @@ const DashboardToggle = () => {
 
 
  const onSignOut = useCallback(() => {
+  
+  database.ref(`/status/${auth.currentUser.uid}`).set(isOfflineForDatabase).then(() =>{
+
+    auth.signOut();
+    
+    Alert.info('Signed Out', 4000);
+    close();
+
+  }).catch(err=> {
+
+    Alert.error(err.message, 4000);
+  });
+
+  
+  }, [close]);
+  
+    return (
+      <>
+        <Button block color="blue" onClick={open}>
+          <Icon icon="dashboard" /> Dashboard
+        </Button>
+        <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
+        <Dashboard onSignOut = {onSignOut}/>
+        </Drawer>
+      </>
+    );
+  };
+  
+  export default DashboardToggle;
+
+
+    
+
+
  
-  auth.signOut();
- 
-  Alert.info('Signed Out', 4000);
 
-  close();
-
-}, [close]);
-
-  return (
-    <>
-      <Button block color="blue" onClick={open}>
-        <Icon icon="dashboard" /> Dashboard
-      </Button>
-      <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
-      <Dashboard onSignOut = {onSignOut}/>
-      </Drawer>
-    </>
-  );
-};
-
-export default DashboardToggle;
