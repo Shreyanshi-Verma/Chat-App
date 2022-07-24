b0816d80111ce86e0fb0a0537b17bb0f5f97eef3
Shreyanshi Verma <shreyanshi.verma200302@gmail.com>
2022-07-25 03:37:41 +0530

    message deletion and updation

--- src/components/chat-window/messages/MessageItem.jsx
+++ src/components/chat-window/messages/MessageItem.jsx
@@ -6,14 +6,15 @@ import ProfileAvatar from '../../ProfileAvatar';
 import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 import {useCurrentRoom} from '../../../context/current-room.context'
 import  {auth} from '../../../misc/firebase';
-import { useHover } from '../../../misc/custom-hooks';
+import { useHover,useMediaQuery  } from '../../../misc/custom-hooks';
 import IconBtnControl from './IconBtnControl';
 
-const MessageItem = ({message, handleAdmin}) => {
+const MessageItem = ({message, handleAdmin, handleLike, handleDelete}) => {
 
-    const {author, createdAt, text }= message;
+    const {author, createdAt, text, likes, likeCount}= message;
 
     const [selfRef, isHovered] = useHover();
+    const isMobile = useMediaQuery('(max-width: 992px)');
 
     const isAdmin = useCurrentRoom(v => v.isAdmin);
     const admins = useCurrentRoom(v => v.admins);
@@ -22,6 +23,9 @@ const MessageItem = ({message, handleAdmin}) => {
     const isMsgAuthorAdmin = admins.includes(author.uid);
     const isAuthor = auth.currentUser.uid === author.uid;
     const canGrantAdmin = isAdmin && !isAuthor;
+
+ const canShowIcons = isMobile || isHovered;
+    const isLiked = likes && Object.keys(likes).includes(auth.currentUser.uid);
 
   return (
 <li 
@@ -63,14 +67,23 @@ className="font-normal text-black-45 ml-2"
 />
 
 <IconBtnControl
-    {...(true ? { color: 'red' } : {})}
-isVisible
+// eslint-disable-next-line
+    {...(isLiked ? { color: 'red' } : {})}
+isVisible={canShowIcons}
 iconName="heart"
 tooltip="Like this message"
-onClick={() => {}}
-badgeContent={5}
+onClick={() => handleLike(message.id)}
+badgeContent={likeCount}
 />
 
+{isAuthor && (
+          <IconBtnControl
+            isVisible={canShowIcons}
+            iconName="close"
+            tooltip="Delete this message"
+            onClick={() => handleDelete(message.id)}
+          />
+        )}
 
 
 
